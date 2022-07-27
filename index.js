53d37ba6bc095bf62b05d1c56fb953c6889ec648
João Paulo <joaojpmoreira25@gmail.com>
2022-07-27 01:19:26 +0000

    feat: returning argument & executing git commands

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 const readline = require('readline-sync')
 const shell = require('shelljs')
 
+function start(){
 
-readline.question("Comando git:")
+    const gitjs = {}
 
-shell.exec("git -v")
+    gitjs.branchName = askAndReturnBranchName()
+    
+    function askAndReturnBranchName(){
+        return readline.question("Escreva o nome da branch: ")
+    }
+
+    function createNewBranch(branch){
+        console.log(`\nrun: git checkout -b "${branch}"`)
+
+        shell.exec(`git checkout -b "${branch}"`)
+    }
+
+    createNewBranch(gitjs.branchName)
+
+    function initNewCommit(){
+        shell.exec("git status")
+        shell.exec("\ngit add --all")
+
+        const commitText = readline.question("Escreva seu comentario?: ")
+
+        shell.exec(`git commit -m "${commitText}"`)
+
+        shell.exec(`git push origin ${branchName}`)
+    }
+
+    initNewCommit()
+}
+
+start()
